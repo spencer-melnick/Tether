1d8bdae9fcb46dd62cf34121af3bfac7927a27b4
spencer-melnick <smelnick97@gmail.com>
2021-06-11 20:48:25 -0400

    Initial project setup

---
+++ Source/Tether/TetherGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TetherGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TETHER_API ATetherGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
