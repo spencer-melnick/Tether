d9b84d246cbac9993b80bb6f171b2b8767f64b89
spencer-melnick <smelnick97@gmail.com>
2021-06-28 17:12:38 -0400

    Adding game instance class, changing PIE local player behavior

---
+++ Source/Tether/Core/TetherGameInstance.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 Spencer Melnick, Stephen Melnick
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TetherGameInstance.generated.h"
+
+UCLASS()
+class UTetherGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual FGameInstancePIEResult InitializeForPlayInEditor(int32 PIEInstanceIndex, const FGameInstancePIEParameters& Params) override;
+};
